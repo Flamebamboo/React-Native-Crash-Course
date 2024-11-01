337c58c40518283efbcd480b94086e602f2299da
Flamebamboo <166450271+Flamebamboo@users.noreply.github.com>
2024-11-01 16:14:25 +1300

    React-Native-Project

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import HugeList from "./components/HugeComponent";
+
+const index = () => {
+  return (
+    <View>
+      <HugeList />
+    </View>
+  );
+};
+
+export default index;
